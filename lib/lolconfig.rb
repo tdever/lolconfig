c77796b531e2eab0f576a8decdaa1a68c07e9595
Tim Dever <tdever@users.noreply.github.com>
2014-09-21 21:24:24 -0500

    implemented some basic config stuff; Testing things out

--- lib/lolconfig.rb
+++ lib/lolconfig.rb
@@ -1,6 +1,71 @@
-require "lolconfig/version"
 require "yaml"
+require "./lolconfig/version"
+require "./lolconfig/config"
 
-module Lolconfig
-  # Your code goes here...
+require "pry"
+
+module Lolconfig	
+
+	binding.pry
+
+	@configs = {
+	}	
+
+
+	def self.set_config(options = {}, name = "default")
+		binding.pry
+		
+		cfg = @configs[name]
+		if cfg
+			cfg.configure(options)
+		else
+			@configs[name] = Config.new(options)
+		end
+	end
+
+
+	def self.load(filename, name = "default")
+		return unless filename
+
+		begin
+			options = YAML::load(IO.read(filename))
+
+			cfg = @configs[name]
+			if cfg
+				cfg.configure(options)
+			else
+				cfg = Config.new(options)	
+			end
+
+		rescue Errno::ENOENT
+			log(:warning, "Configuration file not found.")
+		rescue Psych::SyntaxError
+			log(:warning, "YAML file contains invalid syntax.")
+		end
+	end
+
+
+	#some tests
+	
+	
+	self.set_config({
+		:test1 => "hello",
+		:test2 => "world!"
+	})
+
+
+	if @configs
+		@configs.each { |k, v|
+			if v
+				puts v
+
+				v.each { |k2, v2|
+					puts "{k2}: {v2}"
+				}
+			end
+		}
+	end
+
 end
+
+
